e266a16d440a0bd0884debc5a7d4668371a99b26
Arthur Police <arthurpolice1@gmail.com>
2022-09-15 15:24:48 -0400

    Search engine working

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -20,16 +20,17 @@ def entry(request, title):
            "text": markdown2.markdown(entry_markdown)
         })
     else:
-        return render(request, "encyclopedia/entry_page.html", {
-            "text": "No entries found"
+        suggestions = []
+        for entry in util.list_entries():
+            casefold_entry = entry.casefold()
+            if casefold_entry.count(title.casefold()) > 0:
+                suggestions += [entry]
+        suggestions.sort()
+        return render(request, "encyclopedia/search_results.html", {
+            "entries": suggestions,
+            "query": title
         })
 
 def search(request):
     query = request.GET.get('q')
-    try:
-        return entry(request, query)
-    except:
-        return render(request, "encyclopedia/search_results.html", {
-            "entries": util.list_entries(),
-            "query": query
-        })
+    return entry(request, query)

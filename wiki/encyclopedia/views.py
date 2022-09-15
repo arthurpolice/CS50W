bf74d634a396510d365b680330bbbea2761d11b1
Arthur Police <arthurpolice1@gmail.com>
2022-09-15 10:52:43 -0400

    Entries and Index pages HTML done

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,3 +1,4 @@
+import markdown2
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +9,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry (request, title):
+    entry_markdown = util.get_entry(title)
+    
+    if entry_markdown != None:
+       return render(request, "encyclopedia/entry_page.html",  {
+           "title": title,
+           "text": markdown2.markdown(entry_markdown)
+        })
+    else:
+        return render(request, "encyclopedia/entry_page.html", {
+            "text": "No entries found"
+        })

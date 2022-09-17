64737399171e90bcef0e8479a7cf1e7501672f4d
Arthur Police <arthurpolice1@gmail.com>
2022-09-16 23:45:28 -0400

    Comments added

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -19,12 +19,14 @@ def index(request):
 def entry(request, title):
     entry_markdown = util.get_entry(title)
     
+    # If entry has markdown text, render its page.
     if entry_markdown != None:
        return render(request, "encyclopedia/entry_page.html",  {
            "title": title,
            "text": markdown2.markdown(entry_markdown),
            "markdown": entry_markdown
         })
+    # If user is looking for a non-existing page through search, give him suggestions of pages that contain his search query somehow. 
     else:
         suggestions = []
         for entry in util.list_entries():
@@ -37,6 +39,7 @@ def entry(request, title):
             "query": title
         })
 
+# Get query and send it to entry().
 def search(request):
     query = request.GET.get('q')
     return entry(request, query)
@@ -45,16 +48,20 @@ def new_page(request):
     if request.method == "POST":
         title = request.POST.get("title")
         content = request.POST.get("content")
+        # Save old title for naming the entry.
         old_title = title
+        # Casefold the title as new_title for comparing to casefolded items from existing entries.
         new_title = title.casefold()
         existing_titles = util.list_entries()
         new_existing_titles = []
-        
+        # Casefold existing entries' titles.
         for item in existing_titles:
             new_existing_titles += [item.casefold()]
+        # If the title of the current entry isn't found, let the user save this new entry.
         if new_title not in new_existing_titles:
             title = old_title.capitalize()
             util.save_entry(old_title, content)
+        # If it's found, render an error message.
         else:
             return render (request, "encyclopedia/new_page.html", {
                 "error": "Entry already exists.",
@@ -62,9 +69,9 @@ def new_page(request):
                 "page": "encyclopedia:new_page",
                 "button": "Submit Entry"
             })
-
+        # Redirect user to the entry page upon success.
         return HttpResponseRedirect(reverse("encyclopedia:entry", args=[old_title]))
-    
+    # Renders the page through GET, to render the input fields.
     else:
         return render(request, "encyclopedia/new_page.html", {
             "button": "Submit Entry",
@@ -72,6 +79,8 @@ def new_page(request):
         })
 
 def edit_page(request):
+    # Send these two pieces of information to the edit_page.html, which is a slight variation of new_page.html. 
+    # It will then be sent to the edit route after the changes.
     title = request.POST.get("title")
     content = request.POST.get("content")
     return render(request, "encyclopedia/edit_page.html", {
@@ -80,14 +89,18 @@ def edit_page(request):
         "button": "Edit Entry",
         "page": "encyclopedia:edit"
     })
-    
+
+# The edit route, unlike the new_page route, doesn't stop users from submiting "entries that already exist".
+# To prevent unwanted behaviour with the entry's title, the title input field was set to read only in edit_page.html.
 def edit(request):
     title = request.POST.get("title")
     content = request.POST.get("content")
     util.save_entry(title, content)
-    print(title)
+    # Redirect user to entry's page upon success.
     return HttpResponseRedirect(reverse("encyclopedia:entry", args=[title]))
 
 def randomize(request):
+    # Choose a random title from the list of entries. 
     title = random.choice(util.list_entries())
+    # Redirect user to entry's page upon success.
     return HttpResponseRedirect(reverse("encyclopedia:entry", args=[title]))

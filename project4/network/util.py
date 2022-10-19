4436bf8316d2e0df528c1f9757bc3063efecacde
Arthur Police <arthurpolice1@gmail.com>
2022-10-19 11:18:36 -0400

    More comments.

--- project4/network/util.py
+++ project4/network/util.py
@@ -13,6 +13,7 @@ def follow_status_check(request, requested_profile):
     if requested_profile == request.user:
         follow_status = False
     else:
+        # If this try succeeds, the requested profile is in the list of followed users.
         try:
             requested_profile.followers.get(follower=request.user)
             follow_status = True
@@ -22,6 +23,7 @@ def follow_status_check(request, requested_profile):
 
 
 def get_avatar(requested_profile):
+    # If this try succeeds, the user has an avatar.
     try:
         avatar = Avatar.objects.get(user=requested_profile)
         avatar = avatar.image_url
@@ -31,6 +33,7 @@ def get_avatar(requested_profile):
 
 
 def like_handler_post(current_user, liked_object):
+    # If this try succeeds, the post was liked by the current user already. So that means the user is clicking the button to unlike.
     try:
         liked_object.likes.get(like=current_user)
         PostLike.remove_like_post(liked_object, current_user)
@@ -39,16 +42,21 @@ def like_handler_post(current_user, liked_object):
 
 
 def like_handler_comment(current_user, liked_object):
+        # If this try succeeds, the comment was liked by the current user already. So that means the user is clicking the button to unlike.
     try:
         liked_object.likes.get(like=current_user)
         CommentLike.remove_like_comment(liked_object, current_user)
     except:
         CommentLike.add_like_comment(liked_object, current_user)
 
+# Returns the serialized list of comments related to a post.
 def get_comments(request, post):
+    # Get the ids from the many to many field.
     reply_section = post.replies.all().values('comments')
+    # Use the ids to get the comments.
     comments = Comment.objects.filter(pk__in=reply_section)
     comments = comments.order_by("-timestamp")
+    # serialize() returns a dictionary of relevant information about the comment.
     comments_list = [comment.serialize(request.user) for comment in comments]
     return comments_list
     
